897619e99be3b13c59ab369b0ba09e397e559d04
praveen <praveenjb01@gmail.com>
2026-09-21 17:05:30 -0700

    Add product feature

--- app/app.py
+++ app/app.py
@@ -2,4 +2,8 @@ def payment():
     return "Payment successful"
 
 
+def product():
+    return "Retail product"
+
+
 print(payment())
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,8 +1,15 @@
-from app.app import payment
+from app.app import payment, product
 
 
 def test_payment():
     actual = payment()
     expected = "Payment successful"
+
+    assert actual == expected
+
+
+def test_product():
+    actual = product()
+    expected = "Retail product"
 
     assert actual == expected
